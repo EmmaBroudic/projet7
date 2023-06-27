0aa79087ddb9f67aa380a2048bf91558bf33f420
EmmaBroudic <emma.broudic35@gmail.com>
2023-06-27 16:45:28 +0200

    étape 1.3 en cours

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const mongoose = require('mongoose');
 
-const Thing = require('./models/Thing');
+const booksRoutes = require('./routes/books');
 
 mongoose.connect('mongodb+srv://Poppie:chat2@cluster0.asnsphz.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -21,49 +21,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.post('/Api/books', (req, res, next) => {
-    /*console.log(req.body);
-  res.status(201).json(
-    { message: 'objet créé' }
-  );*/
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-    .catch(error => res.status(400).json({ error }));
-    console.log(thing);
-    next();
-});
+/*app.use(bodyParser.json());*/
 
-app.put('Api/books/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
-    .catch(error => res.status(400).json({ error }));
-    next();
-});
 
-app.delete('Api/books/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
-    .catch(error => res.status(400).json({ error }))
-});
-
-app.get('/Api/books/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-    next();
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-    next();
-});
-
+app.use('Api/books', booksRoutes);
 
 app.use((req, res) => {
     console.log("réponse envoyée avec succès")
---
+++ routes/books.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+const booksCtrl = require('../controllers/books');
+
+const Thing = require('../models/Thing');
+
+router.post('/', booksCtrl.createThing);
+  
+router.put('/:id', booksCtrl.modifyThing);
+
+router.delete('/:id', booksCtrl.deleteThing);
+
+router.get('/:id', booksCtrl.getOneThing);
+
+router.get('/', booksCtrl.getAllThing);
+
+module.exports = router;
